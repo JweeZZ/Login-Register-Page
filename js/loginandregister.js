7ec248ec27f2add7353c5577e5a90ff4351a9656
JweeZZ <r.emreerdogan38@gmail.com>
2022-03-22 17:47:04 +0300

    Login-Register-Page

---
+++ js/loginandregister.js
@@ -0,0 +1,14 @@
+let a = document.getElementById("picture");
+let b = document.getElementById("btn");
+let c = document.getElementById("button-box");
+
+function login(){
+    a.style.left = "42%";
+    b.style.left = "0px";
+    c.style.left = "0px";
+}
+ function register(){
+    a.style.left = "-13%";
+    b.style.left = "110px";
+    c.style.left = "470px";
+}
